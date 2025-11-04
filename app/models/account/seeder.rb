8c5006152cde400314421f681c4b97ea9754b560
Jason Zimdars <jz@37signals.com>
2025-11-04 12:51:38 -0600

    Add the rest

--- app/models/account/seeder.rb
+++ app/models/account/seeder.rb
@@ -39,26 +39,38 @@ class Account::Seeder
 
       playground.cards.create! creator: creator, title: "Grab the invite link to invite someone else", status: "published", description: <<~HTML
         <p>Open Fizzy menu, select “<b><strong>+ Add people</b></strong>”, then copy the invite link. You can give this link to someone else so they can make an login for themselves in your account.</p>
+        <p><br></p>
+        <action-text-attachment url="https://videos.37signals.com/fizzy/assets/images/invite-link.gif" alt="Demo of copying invite link" caption="Get a link to invite co-workers" content-type="image/*" filename="invite-link.gif" presentation="gallery"></action-text-attachment>
       HTML
 
       playground.cards.create! creator: creator, title: "Head back home to check out activity", status: "published", description: <<~HTML
         <p>Hit “1” or pull down the Fizzy menu and select “Home”.</p>
+        <p><br></p>
+        <action-text-attachment url="https://videos.37signals.com/fizzy/assets/images/back-to-home.gif" alt="Demo of visiting Home" caption="Go back to Home for Latest Activity" content-type="image/*" filename="back-to-home.gif" presentation="gallery"></action-text-attachment>
       HTML
 
       playground.cards.create! creator: creator, title: "Check out all cards assigned to you", status: "published", description: <<~HTML
         <p>Pull down the Fizzy menu at the top of the screen, and select “<b><strong>Assigned to me</b></strong>” or just hit “3” on your keyboard any time.</p>
+        <p><br></p>
+        <action-text-attachment url="https://videos.37signals.com/fizzy/assets/images/all-assigned.gif" alt="Demo of navigating to 'Assigned to Me'" caption="See all cards assigned to me" content-type="image/*" filename="all-assigned.gif" presentation="gallery"></action-text-attachment>
       HTML
 
       playground.cards.create! creator: creator, title: "Open the Fizzy menu", status: "published", description: <<~HTML
         <p>The Fizzy menu is how you get around the app. Click “<b><strong>Fizzy</b></strong>” at the top of the screen or hit the “J” key on your keyboard to pop it open.</p>
+        <p><br></p>
+        <action-text-attachment url="https://videos.37signals.com/fizzy/assets/images/open-menu.gif" alt="Demo of opening the main menu" caption="Open the Fizzy menu" content-type="image/*" filename="open-menu.gif" presentation="gallery"></action-text-attachment>
       HTML
 
       playground.cards.create! creator: creator, title: "Assign this card to yourself", status: "published", description: <<~HTML
         <p>Click the little head with the + next to it, pick yourself.</p>
+        <p><br></p>
+        <action-text-attachment url="https://videos.37signals.com/fizzy/assets/images/assign-to-self.gif" alt="Demo of assigning a card" caption="Assign this to yourself" content-type="image/*" filename="assign-to-self.gif" presentation="gallery"></action-text-attachment>
       HTML
 
       playground.cards.create! creator: creator, title: "Tag this card “Design” then move it to YES", status: "published", description: <<~HTML
         <p>Click the little Tag icon, type “design”, then “<b><strong>Create tag</b></strong>”. Then, move the card to the new “YES” column you created in the previous step.</p>
+        <p><br></p>
+        <action-text-attachment url="https://videos.37signals.com/fizzy/assets/images/tag-design.gif" alt="Demo of tagging a card" caption="Tag this #design" content-type="image/*" filename="tag-design.gif" presentation="gallery"></action-text-attachment>
       HTML
 
       playground.cards.create! creator: creator, title: "Make two more columns", status: "published", description: <<~HTML
@@ -70,10 +82,14 @@ class Account::Seeder
         <p>Go back to the Board view, click the little “+” to the right of the DONE column, name the column, pick a color, then do it again.</p>
         <p><br></p>
         <p>After that, drag this card to “DONE” or select “DONE” in the sidebar.</p>
+        <p><br></p>
+        <action-text-attachment url="https://videos.37signals.com/fizzy/assets/images/make-columns.gif" alt="Demo of adding columns" caption="Make two more columns" content-type="image/*" filename="make-columns.gif" presentation="gallery"></action-text-attachment>
       HTML
 
       playground.cards.create! creator: creator, title: "Move this card to NOT NOW", status: "published", description: <<~HTML
         <p>You can either select “NOT NOW” over in the sidebar, or you can go back out to the board view and drag this card into the “NOT NOW” column on the left side.</p>
+        <p><br></p>
+        <action-text-attachment url="https://videos.37signals.com/fizzy/assets/images/not-now.gif" alt="Demo of moving a card to Not Now" caption="Move to Not Now" content-type="image/*" filename="not-now.gif" presentation="gallery"></action-text-attachment>
       HTML
 
       playground.cards.create! creator: creator, title: "Rename this card", status: "published", description: <<~HTML
